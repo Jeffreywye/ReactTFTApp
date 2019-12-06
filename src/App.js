3f45f501ae42ab8f3876fe679329cbaa51b92fa4
Jeffreywye <jeffreywye7@gmail.com>
2019-12-06 05:48:36 -0500

    added rank data retrival

--- src/App.js
+++ src/App.js
@@ -1,40 +1,54 @@
 import React, {useState} from 'react';
+import Player from "./components/Player";
 import './App.css';
 require('dotenv').config();
 
 const App = () => {
-  const [_player, setPlayer] = useState("");
-  const [_search, setSearch] = useState("");
+  const [_player_data, setPlayerData] = useState(null);
+  const [_rank_data, setRankData] = useState(null);
+  var _search = "";
 
   const API_KEY = process.env.REACT_APP_RIOT_API_KEY;
-  const get_player_request = `/tft/summoner/v1/summoners/by-name/${_search}?api_key=${API_KEY}`;
-  
-  
-  const updateSearch = (event) => {
-    setSearch(event.target.value);
-  }
   
   const getPlayerQuery = async (event) => {
     event.preventDefault();
-    setPlayer(_search);
-    console.log(_player);
+    _search = event.target.children.PlayerName.value;
+    const get_player_request = `/tft/summoner/v1/summoners/by-name/${_search}?api_key=${API_KEY}`;
+    // console.log(_player);
     const player_api_call = await fetch(get_player_request);
     const player_data = await player_api_call.json();
     console.log(player_data);
-    setSearch("");
+
+    //// fetch Player rank from API
+    const req_player_rank = `/tft/league/v1/entries/by-summoner/${player_data.id}?api_key=${API_KEY}`;
+    const rank_api_call = await fetch(req_player_rank);
+    const rank_data = await rank_api_call.json();
+    
+    if (rank_data.length === 0){
+      console.log("no ranked player")
+    }
+    else {
+      setPlayerData(player_data);
+      setRankData(rank_data[0]);
+    }
+    document.getElementById("search-form").reset();
+    // // setSearch("");
   }
 
+  // rerender will occur 
+  // whenever a state or prop value changes
   return (
     <div className="App">
       <form
-        className="search-form"
+        id="search-form"
         onSubmit={getPlayerQuery}
       >
         <input
           className="search-bar"
           type="text"
-          value={_search}
-          onChange={updateSearch}
+          // value={_search}
+          // onChange={updateSearch}
+          name = "PlayerName"
         />
         <button
           className="search-btn"
@@ -43,6 +57,16 @@ const App = () => {
           search
         </button>
       </form>
+      
+      { 
+        // render player component when _search is not null
+        _rank_data &&
+        <div>
+          <Player
+            data = {_rank_data}
+          />
+        </div>
+      }
     </div>
   );
 }
--- src/components/player.js
+++ src/components/player.js
@@ -1,8 +1,23 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 require('dotenv').config();
 
-const Player = () => {
+const Player = (props) => {
+    console.log(props.data);
+    // console.log("player rendered");
+    return (
+        <div className="player-comp">
+            <div className="player-rank">
+                <h1>{props.data.summonerName}</h1>
+                <button 
+                    type="button">
+                    See Past 10 Matches
+                </button>
+            </div>
+            <div className="player-matches">
 
+            </div>
+        </div>
+    );
 };
 
 export default Player;
