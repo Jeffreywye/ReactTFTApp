ac43052d6baa74cd7fcef9be5569f065ae8de98b
Jeffreywye <jeffreywye7@gmail.com>
2019-12-10 05:14:30 -0500

    completed method to fetch match objects

--- src/App.js
+++ src/App.js
@@ -6,33 +6,69 @@ require('dotenv').config();
 const App = () => {
   const [_player_data, setPlayerData] = useState(null);
   const [_rank_data, setRankData] = useState(null);
+  const [_display_matches, setDisplayMatches] = useState(true);
+  var _list_of_matches = [];
   var _search = "";
 
   const API_KEY = process.env.REACT_APP_RIOT_API_KEY;
+
+  async function fetchMatches(listOfMatchIDs){
+    for(let i = 0; i<listOfMatchIDs.length; i++){
+      const matchID = listOfMatchIDs[i];
+      console.log(matchID);
+      const req_match = `/matches/${matchID}?api_key=${API_KEY}`;
+      const matchData = await fetchData(req_match);
+      console.log(matchData);
+      _list_of_matches.push(matchData)
+    }
+    
+  }
+
+  async function  fetchData(url){
+    const api_call = await fetch(url);
+    const data = await api_call.json();
+    return data;
+  }
   
   const getPlayerQuery = async (event) => {
     event.preventDefault();
     _search = event.target.children.PlayerName.value;
-    const get_player_request = `/tft/summoner/v1/summoners/by-name/${_search}?api_key=${API_KEY}`;
-    // console.log(_player);
-    const player_api_call = await fetch(get_player_request);
-    const player_data = await player_api_call.json();
-    console.log(player_data);
+    const get_player_request = `/summoner/v1/summoners/by-name/${_search}?api_key=${API_KEY}`;
+    const player_data = await fetchData(get_player_request);
 
-    //// fetch Player rank from API
-    const req_player_rank = `/tft/league/v1/entries/by-summoner/${player_data.id}?api_key=${API_KEY}`;
-    const rank_api_call = await fetch(req_player_rank);
-    const rank_data = await rank_api_call.json();
-    
-    if (rank_data.length === 0){
-      console.log("no ranked player")
+    // fetch Player rank from API
+    const req_player_rank = `/league/v1/entries/by-summoner/${player_data.id}?api_key=${API_KEY}`;
+    const rank_data = await fetchData(req_player_rank);
+    // valid rank data is returned as an array
+    // invalid requests for rank_data returns a json obj
+    if (rank_data.length === 0 || !Array.isArray(rank_data) ){
+      console.log("no ranked player");
     }
+
+    //valid rank player
     else {
+      // clear prev player list of matches
+      _list_of_matches = [];
       setPlayerData(player_data);
       setRankData(rank_data[0]);
+
+      // fetch list of matchIDS
+      let count = 5;
+      const req_player_matches = `/by-puuid/${player_data.puuid}/ids?count=${count}&api_key=${API_KEY}`;
+      const matchIDS = await fetchData(req_player_matches);
+      console.log(matchIDS);
+      await fetchMatches(matchIDS);
+      setMatchDisplayOff();
     }
     document.getElementById("search-form").reset();
-    // // setSearch("");
+  }
+
+  const setMatchDisplayOff = (e) => {
+    setDisplayMatches(false);
+  }
+
+  const toggleMatchDisplay = (e) => {
+    setDisplayMatches(!_display_matches);
   }
 
   // rerender will occur 
@@ -46,8 +82,6 @@ const App = () => {
         <input
           className="search-bar"
           type="text"
-          // value={_search}
-          // onChange={updateSearch}
           name = "PlayerName"
         />
         <button
@@ -64,6 +98,8 @@ const App = () => {
         <div>
           <Player
             data = {_rank_data}
+            display = {_display_matches}
+            displayMatchOnClick = {toggleMatchDisplay}
           />
         </div>
       }
--- src/components/player.js
+++ src/components/player.js
@@ -1,17 +1,27 @@
 import React from 'react';
-require('dotenv').config();
 
 const Player = (props) => {
-    console.log(props.data);
-    // console.log("player rendered");
+    // console.log(props.data);
+    console.log("player rendered");
     return (
         <div className="player-comp">
             <div className="player-rank">
                 <h1>{props.data.summonerName}</h1>
                 <button 
-                    type="button">
+                    type="button"
+                    onClick={props.displayMatchOnClick}>
                     See Past 10 Matches
                 </button>
+                {
+                    props.display &&
+                    <div className="Matches">
+                        <ul>
+                            <li>test</li>
+                            <li>test</li>
+                        </ul>
+                    </div>
+                }
+                
             </div>
             <div className="player-matches">
 
