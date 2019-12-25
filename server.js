636a27eda80563c9c205a99730131958d6ee6aeb
Jeffreywye <jeffreywye7@gmail.com>
2019-12-25 00:40:17 -0500

    uninstalled some dependencies

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+require('dotenv').config();
+
+const app = express();
+app.use(bodyParser.json());
+
+const API_KEY = process.env.API_KEY;
+const PORT = process.env.PORT || 5000;
+
+function fetchData(url, callback){
+    request(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            let data = JSON.parse(body);
+            callback(data); 
+        }
+    });
+}
+
+app.post('/api', async (req, res) =>{
+    let link = req.body.url + API_KEY;
+    fetchData(link, function(data){
+        res.json(data);
+    });
+});
+
+app.listen(PORT, () =>{
+    console.log(`Server started on Port ${PORT}`);
+});
