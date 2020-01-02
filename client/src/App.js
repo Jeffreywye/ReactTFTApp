6d29f092c2d4641460158307bdb8f367134e5cff
Jeffreywye <jeffreywye7@gmail.com>
2020-01-01 20:39:40 -0500

    navbar wont collapse anymore

--- client/src/App.js
+++ client/src/App.js
@@ -434,22 +434,22 @@ const App = () => {
   return (
     <div className="App">
       
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+      <nav className="navbar navbar-expand navbar-dark bg-dark sticky-top" id="mainNav">
         <div className="container">
-          <a className="navbar-brand nav-link js-scroll-trigger" href="#search">TFT APP</a>
+          <a className="navbar-brand nav-link" href="#about">TFT APP</a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarResponsive">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
-                <a className="nav-link js-scroll-trigger" href="#about">About</a>
+                <a className="nav-link" href="#about">About</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link js-scroll-trigger" href="#player-search">Search Player</a>
+                <a className="nav-link" href="#player-search">Search Player</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link js-scroll-trigger" href="#meta-table">Meta</a>
+                <a className="nav-link" href="#meta-table">Meta</a>
               </li>
             </ul>
           </div>
